608135a7d6b878a94d0a37759c2b42a734f6d39c
Jack David Warren <jwarren315@login-ice-4.pace.gatech.edu>
2025-01-08 18:09:35 -0500

    qwen local LLM function

--- src/llm_utils.py
+++ src/llm_utils.py
@@ -7,7 +7,7 @@ import glob
 import time
 import numpy as np
 import transformers
-from torch import bfloat16
+from torch import bfloat16, float16
 from utils.privit import *
 from cfg.constants import *
 from utils.print_utils import box_print
@@ -39,7 +39,10 @@ def generate_augmented_code(txt2llm, augment_idx, apply_quality_control, top_p, 
     print(txt2llm)
     
     if hugging_face is False:
-        llm_code_generator = submit_mixtral
+        if LLM_MODEL == 'mixtral':
+            llm_code_generator = submit_mixtral
+        elif LLM_MODEL == 'qwen':
+            llm_code_generator = submit_qwen
         qc_func = llm_code_qc
     else:
         if LLM_MODEL == 'mixtral':
@@ -205,6 +208,44 @@ def submit_mixtral(txt2mixtral, max_new_tokens=764, top_p=0.15, temperature=0.1,
     )
 
     res = generate_text(txt2mixtral)
+    output_txt = res[0]["generated_text"]
+    box_print("LLM OUTPUT", print_bbox_len=60, new_line_end=False)
+    print(output_txt)
+    box_print(f'time to load in seconds: {round(time.time()-start_time)}', print_bbox_len=120, new_line_end=False)   
+    if return_gen is False:
+        return output_txt
+    else:
+        return output_txt, generate_text
+    
+def submit_qwen(txt2qwen, max_new_tokens=764, top_p=0.15, temperature=0.1, 
+                   model_id="Qwen/Qwen2.5-72B-Instruct", return_gen=False):
+    max_new_tokens = np.random.randint(800, 1000)
+    print(f'max_new_tokens: {max_new_tokens}')
+    start_time = time.time()
+    model = transformers.AutoModelForCausalLM.from_pretrained(
+        model_id,
+        trust_remote_code=True,
+        torch_dtype=float16,
+        device_map='auto'
+    )
+    model.eval()
+    print(model.device)
+    tokenizer = transformers.AutoTokenizer.from_pretrained(model_id)
+
+    generate_text = transformers.pipeline(
+        model=model, tokenizer=tokenizer,
+        return_full_text=False,  # if using langchain set True
+        task="text-generation",
+        # we pass model parameters here too
+        temperature=temperature,  # 'randomness' of outputs, 0.0 is the min and 1.0 the max
+        top_p=top_p,  # select from top tokens whose probability add up to 15%
+        top_k=0,  # select from top 0 tokens (because zero, relies on top_p)
+        max_new_tokens=max_new_tokens,  # max number of tokens to generate in the output
+        repetition_penalty=1.1,  # if output begins repeating increase
+        do_sample=True,
+    )
+
+    res = generate_text(txt2qwen)
     output_txt = res[0]["generated_text"]
     box_print("LLM OUTPUT", print_bbox_len=60, new_line_end=False)
     print(output_txt)
