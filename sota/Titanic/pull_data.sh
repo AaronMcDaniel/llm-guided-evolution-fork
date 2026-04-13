bd7884b0b836a2b532d562a55643b988a870c755
Thinh Nguyen <thinhmnguyen4105@gmail.com>
2026-04-13 14:19:54 -0400

    test push

--- sota/Titanic/pull_data.sh
+++ sota/Titanic/pull_data.sh
@@ -1,15 +1,2 @@
-#!/usr/bin/env bash
-
-set -euo pipefail
-
-mkdir -p data
-
-echo "PWD: $(pwd)"
-echo "Checking Kaggle access..."
-kaggle competitions files -c titanic
-
-echo "Downloading Titanic train.csv..."
-kaggle competitions download -c titanic -f train.csv -p data
-
-echo "Files in data/:"
-ls -lah data
+#!/bin/bash
+kaggle competitions download -c titanic -p data -f train.csv
