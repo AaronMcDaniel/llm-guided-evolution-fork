717efe1d3abff645fe92620da7bdb43cd23fb220
Thomas Park Dean <tdean40@login-ice-2.pace.gatech.edu>
2026-04-20 17:47:25 -0400

    Fix GPU req for slurm-submit-test
    Only including H100/200 as advised

--- slurm-submit-test.sh
+++ slurm-submit-test.sh
@@ -5,7 +5,7 @@
 #SBATCH -N 1
 #SBATCH --mem-per-gpu 16G
 #SBATCH --gres=gpu:1
-#SBATCH -C "A100-40GB|A100-80GB|H100|V100-16GB|V100-32GB|RTX6000|A40|L40S"
+#SBATCH -C "H100|H200"
 #SBATCH -o slurm-%j.out
 #SBATCH -e slurm-%j.err
 
