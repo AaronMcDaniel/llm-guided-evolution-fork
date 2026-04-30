11eca51150e0acd69e174c217b8c7d448ce13560
Temiloluwa M Cole <tcole43@atl1-1-02-010-1-1.pace.gatech.edu>
2026-04-30 13:52:01 -0400

    Forcing benchmark statements to appear

--- slurm-submit-test.sh
+++ slurm-submit-test.sh
@@ -29,10 +29,10 @@ command -v uv >/dev/null 2>&1 || { echo "uv is not installed or not on PATH"; ex
 uv sync
 
 # Run tests inside the uv-managed environment and generate JUnit XML
-uv run pytest tests/test_*.py --junitxml=tests/results/report.xml
+#uv run pytest tests/test_*.py --junitxml=tests/results/report.xml
 
-#Added -v and the capture option  for verbose output and to allow print statement from benchmark test to display in xml report and slurm output 
-#uv run pytest tests/test_*.py -v --capture=tee-sys --junitxml=tests/results/report.xml
+#including "junit_logging=all" will ensure print statements are included
+uv run pytest tests/test_*.py -s --junitxml=tests/results/report.xml -o "junit_logging=all"
 
 echo "==== SLURM JOB END ===="
 date
